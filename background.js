b4758af7703a68da77733ae6f836fc9e0443090f
Jenny Park <sp785@cornell.edu>
2017-03-31 14:37:44 -0400

    Change background page to event page
    Event pages are only loaded when they are needed, and saves memory and other system resources. Refer to https://developer.chrome.com/extensions/event_pages to get detailed information regarding event pages and the proper protocol for the context menus API.

--- background.js
+++ background.js
@@ -1,14 +1,33 @@
 /**
- Create a context menu item with the following properties.
- The context menu item appears only when the user highlights text
+ When the extension is installed or upgraded,
+ create a context menu item with the following properties.
+ The context menu item appears only when the user selects text
  on the page.
 */
-chrome.contextMenus.create({"title": "save highlight", "contexts": ["selection"], "onclick": onClickSave});
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        title: 'highlight',
+        id: 'PageHighlight', // you'll use this in the handler function to identify this context menu item
+        contexts: ['selection'],
+    });
+});
+
+
+/**
+ * The listener for the context menu item with id 'PageHighlight'.
+ * When the user clicks this context menu item,
+ * the selected text is saved to storage.
+ */
+chrome.contextMenus.onClicked.addListener(function(info) {
+	if (info.menuItemId == 'PageHighlight') {
+		onClickSave(info);
+	}
+});
 
 /**
  * When the user clicks "save highlight" in the context menu:
- * 	- the selected text must be put into storage
- *  - the selected text should appear highlighted
+ *	- the selected text must be put into storage
+ *	- the selected text should appear highlighted
  */
 function onClickSave(info) {
 	saveToStorage(info);
@@ -31,3 +50,4 @@ function saveToStorage(info) {
 		chrome.storage.sync.set(newStorageObject);
 	});
 }
+
