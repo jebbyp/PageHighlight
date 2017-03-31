4131096adfccd63be4aca629d8727862292a46ad
Jenny Park <sp785@cornell.edu>
2017-03-31 13:41:05 -0400

    Add saving to storage functionality
    Users will now be able to save their selected texts to storage when they click the "save highlight" context menu item.
    Selected texts are saved based on the page url.

--- background.js
+++ background.js
@@ -1,8 +1,33 @@
+/**
+ Create a context menu item with the following properties.
+ The context menu item appears only when the user highlights text
+ on the page.
+*/
 chrome.contextMenus.create({"title": "save highlight", "contexts": ["selection"], "onclick": onClickSave});
 
 /**
- * When the user clicks "save highlight" in the context menu,
- * the highlighted text must be stored in storage immediately.
- * 
+ * When the user clicks "save highlight" in the context menu:
+ * 	- the selected text must be put into storage
+ *  - the selected text should appear highlighted
  */
-function onClickSave(info) {}
+function onClickSave(info) {
+	saveToStorage(info);
+}
+
+/**
+ * Put the selected text into storage.
+ * Text will be saved by urls in a set.
+ */
+function saveToStorage(info) {
+	pageUrl = info.pageUrl;
+	chrome.storage.sync.get(pageUrl, function(storageObject) {
+		// get existing saved texts on the webpage, if any
+		var savedTexts = storageObject[pageUrl] || [];
+		savedTexts.push(info.selectionText);
+
+		var newStorageObject = {};
+		newStorageObject[pageUrl] = savedTexts;
+
+		chrome.storage.sync.set(newStorageObject);
+	});
+}
