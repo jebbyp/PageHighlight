7ec66a61099975605136ca43a09e5ffd1e812527
Jenny Park <sp785@cornell.edu>
2017-04-01 09:51:29 -0400

    Change <p> to <span>
    <p> produces separate blocks of paragraphs, leading to extraneous
    spacing when highlighting text (especially when the first word is
    sliced). As <span> is inline styling, it will lead to less
    complications with affecting the original style of the webpage.

--- content.js
+++ content.js
@@ -1,15 +1,19 @@
 highlightSavedTexts(document.URL);
 
+/**
+ * When there is a change in a webpage's saved texts,
+ * highlight the most recently saved text
+ */
 chrome.storage.onChanged.addListener(function(changedPages, namespace) {
 	for (pageURL in changedPages) {
 		chrome.storage.sync.get(pageURL, function(storageObject) {
 			highlightSavedTexts(pageURL, 1);
 		});
 	}
-})
+});
 
 /**
- * Highlight previously saved texts of this page.
+ * Highlight previously saved texts of this page
  */
 function highlightSavedTexts(pageURL, count) {
 	chrome.storage.sync.get(pageURL, function(storageObject) {
@@ -37,7 +41,7 @@ function updateHTML(savedText, HTMLElement) {
 	if (startIndex != -1) {
 		var start = HTMLContent.slice(0, startIndex);
 
-		var middle = "<p class='highlight'>" + savedText + "</p>";
+		var middle = "<span class='highlight'>" + savedText + "</span>";
 
 		var endIndex = startIndex + savedText.length;
 		var end = HTMLContent.slice(endIndex);
